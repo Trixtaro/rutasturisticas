5c7d7f870845c0fda11636425150d18992345b20
Luis Suárez <xboxluis2010@hotmail.com>
2019-11-27 00:28:24 -0500

    Se añadio el archivo fuente para la BD

---
+++ Fuente.sql
@@ -0,0 +1,14 @@
+-- Tiempo de generación de este archivo: 27-11-2019 a las 00:12:59
+-- 
+-- Proyecto
+-- Ingeniería de Interfaces A
+
+CREATE DATABASE RutasTuristicas;
+
+USE RutasTuristicas;
+
+-- --------------------------------------------------------
+
+--
+-- Definición de la tabla 
+--
