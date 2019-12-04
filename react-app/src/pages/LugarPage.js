ba84a07d28464606ac94cad869e3d62888b3df74
Trixtaro <wilmerdavid97@hotmail.com>
2019-12-04 16:57:10 -0500

    Se creo el componente lugar y se lo implemento en LugarPage

--- react-app/src/pages/LugarPage.js
+++ react-app/src/pages/LugarPage.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import Lugar from '../components/Lugar';
+import foto from '../images/montecristi.jpg';
+
 import './styles/LugarPage.css';
 
 class LugarPage extends React.Component{
@@ -11,7 +14,18 @@ class LugarPage extends React.Component{
                     Los lugares más visitados
                 </h1>
                 <div className="lugares">
-                    
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                    <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
                 </div>
             </div>
         );
