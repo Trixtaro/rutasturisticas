263aaa34f6596c23532a6334a39be8867f0a0570
Trixtaro <wilmerdavid97@hotmail.com>
2019-12-04 14:32:38 -0500

    Ahora se pueden usar diferentes Layout usado AppRoute

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
+import AppRoute from '../components/AppRoute';
 import Layout from './Layout';
 import Dashboard from '../pages/Dashboard';
 import Login from '../pages/LoginPage';
@@ -11,16 +12,13 @@ function App(){
 
     return (
         <BrowserRouter>
-            <Layout>
-                <Switch>
-
-                    <Route exact path="/" component={Dashboard} />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/register" component={Register} />
-                    <Redirect path="*" to="/404" />
-
-                </Switch>
-            </Layout>
+            <Switch>
+                <AppRoute exact path="/" component={Dashboard} layout={Layout} />
+                <AppRoute exact path="/" component={Dashboard} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+                <Redirect path="*" to="/404" />
+            </Switch>
         </BrowserRouter>
     );
 
