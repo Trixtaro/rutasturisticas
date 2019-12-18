a3bacb42f970ef286a5d574182bdf66679179155
Trixtaro <wilmerdavid97@hotmail.com>
2019-12-18 08:21:10 -0500

    Se creo la api que muestra los lugares

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -4,4 +4,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/api/lugares','LugarController@index');
+
 Route::post('/usuario/{tipo_usuario}', 'UsuarioController@store');
