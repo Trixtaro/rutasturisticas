21ce94f890c81d7ee46bf78fbf37853abad7f262
Trixtaro <wilmerdavid97@hotmail.com>
2019-12-17 20:13:08 -0500

    Se agrego el css necesario para el autocomplete

--- react-app/src/components/Buscador.js
+++ react-app/src/components/Buscador.js
@@ -7,11 +7,13 @@ class Buscador extends React.Component{
 
     render(){
         return (
-            <form className="Buscador" action="">
-                <input type="text" placeholder={this.props.placeholder} />
-                <Link to="">
-                    <i className="fas fa-search lupa"></i>
-                </Link>
+            <form autoComplete="off" className="" action="">
+                <div className="Buscador autocomplete">
+                    <input id="buscador" type="text" placeholder={this.props.placeholder} />
+                    <button className="lupa" type="submit">
+                        <i className="fas fa-search"></i>
+                    </button>
+                </div>
             </form>
         );
     }
