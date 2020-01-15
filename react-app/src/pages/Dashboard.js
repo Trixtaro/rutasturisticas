9e46c221c8a7b5e19443a4191c463b2fd3c63c63
Trixtaro <wilmerdavid97@hotmail.com>
2020-01-14 21:09:05 -0500

    Se mejoro el css del dashboard
    Se agregaron nuevas imagenes de ejemplo

--- react-app/src/pages/Dashboard.js
+++ react-app/src/pages/Dashboard.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 import Buscador from '../components/Buscador';
 import Lugar from '../components/Lugar';
-import foto from '../images/montecristi.jpg';
+import montecristi from '../images/montecristi.jpg';
+import portoviejo from '../images/portoviejo.jpg';
+import manta from '../images/manta.jpg';
 
 import './styles/Dashboard.css';
 
@@ -14,16 +16,16 @@ class Dashboard extends React.Component {
                     <h1>Rutas Turísticas</h1> 
                     <p>Encuentra tu guía  turistico <br /> para ese viaje que tanto deseas.</p>
                     <div className="Dashboard-buscador">
-                        <Buscador placeholder="Ingrese el lugar" />
+                        <Buscador placeholder="Busca tus lugares preferidos..." />
                     </div>
                     <div>
                         <h2 className="titulo-lugares">
                             Los lugares más visitados del mes
                         </h2>
                         <div className="Dashboard-lugares">
-                            <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
-                            <Lugar clases={'selected'} titulo="Manta" imagen={foto} descripcion="La capital de los pescados" />
-                            <Lugar titulo="Portoviejo" imagen={foto} descripcion="La capital de los manabitas" />
+                            <Lugar titulo="Portoviejo" imagen={portoviejo} descripcion="La capital de los manabitas." />
+                            <Lugar titulo="Manta" imagen={manta} descripcion="Principal puerto atunero del mundo." />
+                            <Lugar titulo="Montecristi" imagen={montecristi} descripcion="La cuna de Eloy Alfaro, y artesanias como el sombrero de paja toquilla." />
                         </div>
                     </div>
                     <div className="points">
