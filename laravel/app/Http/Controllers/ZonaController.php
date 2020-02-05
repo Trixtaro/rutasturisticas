59f7ea8ec14669c356daa33a6fe9c7794715d138
Cinthya Briones <cinthya2797@gmail.com>
2020-02-05 11:31:30 -0500

    Creo modelo y controlador de zona

---
+++ laravel/app/Http/Controllers/ZonaController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Resources\LugarResource;
+use App\Http\Resources\LugarResource2;
+
+class ZonaController extends Controller {
+
+    public function index () {
+        //
+    }
+
+    public function create () {
+        //
+    }
+
+    public function store (Request $request) {
+        //
+    }
+
+    public function show ($id_lugar) {
+        //
+    }
+
+    public function edit ($id) {
+        //
+    }
+
+    public function update (Request $request, $id) {
+        //
+    }
+
+    public function destroy ($id) {
+        //
+    }
+
+    public function search ($id_lugar) {
+        //
+    }
+}
