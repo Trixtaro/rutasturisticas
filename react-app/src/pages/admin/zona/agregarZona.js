c9a44910faa680768e928122496a9dbce7b3ac5b
Cinthya Briones <cinthya2797@gmail.com>
2020-02-12 13:23:27 -0500

    Se mejoró CSS de zona

--- react-app/src/pages/admin/zona/agregarZona.js
+++ react-app/src/pages/admin/zona/agregarZona.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 class agregarZona extends React.Component {
     constructor (props) {
@@ -49,38 +50,65 @@ class agregarZona extends React.Component {
 
     formulario () {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <div className="input-group input-group-sm mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="inputGroup-sizing-sm">Latitud</span>
+            <div className="container-sm">
+                <h1>Agregue <span class="badge badge-secondary">Zona</span></h1>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                      <li class="breadcrumb-item"><Link to="/admin/home">Home</Link></li>
+                      <li class="breadcrumb-item"><Link to="/admin/zona">Zonas</Link></li>
+                      <li class="breadcrumb-item active" aria-current="page">Nuevo</li>
+                    </ol>
+                </nav>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="row">
+                        <div className="col">
+                            <div class="form-group">
+                                <label for="InputLatitud">Latitud</label>
+                                <input type="text" class="form-control" id="InputLatitud" onChange={this.handleChange} value={this.state.form.latitud} required aria-describedby="latitudHelp" />
+                                <small id="latitudHelp" class="form-text text-muted"></small>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div class="form-group">
+                                <label for="InputLongitud">longitud</label>
+                                <input type="text" class="form-control" id="InputLongitud" onChange={this.handleChange} value={this.state.form.longitud} required aria-describedby="longitudHelp" />
+                                <small id="longitudHelp" class="form-text text-muted"></small>
+                            </div>
+                        </div>
                     </div>
-                    <input type="text" id="latitud" name="latitud" onChange={this.handleChange} value={this.state.form.latitud} required/>
-                    
-                </div>
 
-                
-                <div className="field">
-                    <input type="text" id="longitud" name="longitud" placeholder="Longitud"  onChange={this.handleChange} value={this.state.form.longitud} required/>
-                </div>
-                <div className="field">
-                    <input type="text" id="nombre" name="nombre" placeholder="Nombre"  onChange={this.handleChange} value={this.state.form.nombre} required/>
-                </div>
-                <div className="field">
-                    <select id="cargo" name="cargo" placeholder="Cargo" onChange={this.handleChange} value={this.state.form.cargo} required>
-                        <option value="">Seleccione un cargo...</option> 
-                        <option value="Pais">País</option>
-                        <option value="Provincia">Provincia</option>
-                        <option value="Canton">Cantón</option>
-                        <option value="Parroquia">Parroquia</option>
-                        <option value="Parroquia Rural">Parroquia Rural</option>
-                        <option value="Parroquia Urbana">Parroquia Urbana</option>
-                    </select>
-                </div>
-                <div className="field">
-                    <input type="text" id="descripcion" name="descripcion" placeholder="Descripción"  onChange={this.handleChange} value={this.state.form.descripcion} required/>
-                </div>
-                <button className="boton">Agregar Lugares</button>
-            </form>
+                    <div className="row">
+                        <div className='col'>
+                            <div class="form-group">
+                                <label for="InputNombre">Nombre</label>
+                                <input type="text" class="form-control" id="InputNombre" onChange={this.handleChange} value={this.state.form.nombre} required onChange={this.handleChange} value={this.state.form.longitud} required aria-describedby="nombreHelp" />
+                                <small id="nombreHelp" class="form-text text-muted"></small>
+                            </div>
+                        </div>                        
+                        <div className='col'>
+                            <div class="form-group">
+                                <label for="cargoSelect">Cargo</label>
+                                <select class="form-control" id="cargoSelect" onChange={this.handleChange} value={this.state.form.cargo} required >
+                                    <option value="">Seleccione un cargo...</option> 
+                                    <option value="Pais">País</option>
+                                    <option value="Provincia">Provincia</option>
+                                    <option value="Canton">Cantón</option>
+                                    <option value="Parroquia">Parroquia</option>
+                                    <option value="Parroquia Rural">Parroquia Rural</option>
+                                    <option value="Parroquia Urbana">Parroquia Urbana</option>
+                                </select>
+                            </div>
+                        </div>                                        
+                    </div>
+ 
+                    <div class="form-group">
+                        <label for="descripcionTextarea">Descripción</label>
+                        <textarea class="form-control" id="descripcionTextarea" rows="3" onChange={this.handleChange} value={this.state.form.descripcion} required ></textarea>
+                    </div>
+
+                    <button type="button" className="btn btn-primary">Agregar Zona</button>
+                </form>
+            </div>
         );
     }
 
